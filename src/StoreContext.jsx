717af7e60503224ce78469d3f39bd5d059513725
Mahender-Reddy-ui <mahender7832@gmail.com>
2025-08-28 12:36:44 +0530

    Update StoreContext.jsx

--- src/StoreContext.jsx
+++ src/StoreContext.jsx
@@ -5,7 +5,6 @@ export const StoreContext = createContext();
 export const StoreProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token") || "");
   const url = "https://backendfinalb32-jyxw.onrender.com"; 
-"; 
   return (
     <StoreContext.Provider value={{ token, setToken, url }}>
       {children}
